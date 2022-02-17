1eac8f24913e056d717131919ef373f00ce2525e
Peiran Lyu <2612736l@student.gla.ac.uk>
2022-02-17 18:48:32 +0000

    modified condition of remove highlight

--- app/events/CardClicked.java
+++ app/events/CardClicked.java
@@ -24,27 +24,30 @@ import utils.StaticConfFiles;
  */
 public class CardClicked implements EventProcessor{
 
+
 	@Override
 	public void processEvent(ActorRef out, GameState gameState, JsonNode message) {
-		
+
 		int handPosition = message.get("position").asInt();
 
 		Card clickedCard = gameState.getHumanPlayer().getHand().getHandList().get(handPosition);
-
+		Card preClickedCard = gameState.getCardSelected();
 
 		//firstly remove the highlight on the board
-		if(gameState.getCardSelected() != null && gameState.getUnitSelected() != null){
+		if(preClickedCard == clickedCard){
 			removeHighlight(out, gameState);
 			gameState.setCardSelected(null);
 		}
 		//first justify the card type(attack or assist), then determine the highlight area
 		else {
+			Avatar humanAvator = gameState.getHumanAvatar();
+			Position pos = humanAvator.getPosition();
 			if(clickedCard.getId() == 8 || clickedCard.getId() == 9){
 				//spell card type, complete later
 				//TODO
 			}else{
 				//unit type card
-				highlight(out, gameState);
+				nonSpellHighlight(out, gameState, pos);
 				gameState.setCardSelected(clickedCard);
 			}
 
@@ -68,19 +71,16 @@ public class CardClicked implements EventProcessor{
 
 	}
 
-	private void highlight(ActorRef out, GameState gameState){
-		Avatar humanAvator = gameState.getHumanAvatar();
-		int tilex = humanAvator.getPosition().getTilex();
-		int tiley = humanAvator.getPosition().getTiley();
+	private void nonSpellHighlight(ActorRef out, GameState gameState,Position position){
 		int x_max = gameState.gameBoard.getGameBoard().length;
 		int y_max = gameState.gameBoard.getGameBoard()[0].length;
-		for (int i=tilex-1; i<tilex+2; i++) {
-			for (int j = tiley - 1; j < tiley + 2; j++) {
+		for (int i=position.getTilex()-1; i< position.getTilex()+2; i++) {
+			for (int j = position.getTiley() - 1; j < position.getTiley() + 2; j++) {
 				// make sure the tile is on the board
 				if (i >= 0 && i<x_max && j >= 0 && j<y_max) {
 					Monster otherUnit = gameState.gameBoard.getGameBoard()[j][i].getUnitOnTile();
 					if (otherUnit!=null) {
-						BasicCommands.drawTile(out, gameState.gameBoard.getGameBoard()[j][i], 2);
+						BasicCommands.drawTile(out, gameState.gameBoard.getGameBoard()[j][i], 0);
 					}else{
 						BasicCommands.drawTile(out, gameState.gameBoard.getGameBoard()[j][i], 1);
 					}
